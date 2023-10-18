49aeefcfd9c96495665d9a6daa54246ba673b9fd
Ed Wright <ewright@digitalfutures.com>
2023-10-18 10:57:37 +0100

    TRY: All but services tests removed

--- tests/Footer.test.jsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import Footer from '../src/Components/Footer';
-
-test(`Footer matches snapshot`, () => {
-  const footerComponent = render(<Footer />);
-
-  expect(footerComponent).toMatchSnapshot();
-});
